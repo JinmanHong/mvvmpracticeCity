810a9f8f627292420f3d57407f606ae94620baa2
jinman hong <jinmanhong@Juliaui-MacBookAir.local>
2022-11-15 23:19:43 +0100

    Initial commit

---
+++ app/src/main/java/com/example/mvvmpractice1/view/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.mvvmpractice1.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.core.content.res.ResourcesCompat
+import androidx.lifecycle.Observer
+import com.example.mvvmpractice1.databinding.ActivityMainBinding
+import com.example.mvvmpractice1.viewmodel.CityViewModel
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private val model: CityViewModel by viewModels()
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        model.getCityData().observe(this, Observer { city ->
+            binding.cityImage.setImageDrawable(
+                ResourcesCompat.getDrawable(resources, city.img, applicationContext.theme)
+            )
+            binding.cityNameTV.text = city.name
+            binding.cityPopulationTV.text = city.population.toString()
+        })
+    }
+}
+
+
+
